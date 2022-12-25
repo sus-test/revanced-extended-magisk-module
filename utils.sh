036916a550f0993a0e2edc476733b1a2cc3f89c8
MatadorProBr <71022656+MatadorProBr@users.noreply.github.com>
2022-12-25 19:55:19 -0300

    Fix some things

--- utils.sh
+++ utils.sh
@@ -170,7 +170,7 @@ get_uptodown_pkg_name() {
 patch_apk() {
 	local stock_input=$1 patched_apk=$2 patcher_args=$3
 	declare -r tdir=$(mktemp -d -p $TEMP_DIR)
-	local cmd="java -jar $RV_CLI_JAR --rip-lib x86_64 --rip-lib x86 --temp-dir=$tdir -c -a $stock_input -o $patched_apk -b $RV_PATCHES_JAR --keystore=ks.keystore $patcher_args"
+	local cmd="java -jar $RVX_CLI_JAR --rip-lib x86_64 --rip-lib x86 --temp-dir=$tdir -c -a $stock_input -o $patched_apk -b $RVX_PATCHES_JAR --keystore=ks.keystore $patcher_args"
 	echo "$cmd"
 	if [ $DRYRUN = true ]; then
 		cp -f "$stock_input" "$patched_apk"
